4f783f6e2dc2ffe8a9655f895ecd96a2be83c445
ZhongylHNU <2723084694@qq.com>
2023-08-20 10:32:38 +0800

    前后端分离写法，vue配置及写法

--- backend/src/main/java/com/kob/backend/controller/pk/returnInfo.java
+++ backend/src/main/java/com/kob/backend/controller/pk/returnInfo.java
@@ -4,12 +4,26 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class returnInfo {
 
     @RequestMapping("index")
-    public String index1() {
-        return "ERROR : the url should end with '/'";
+public String index1() {
+    return "ERROR : the url should end with '/'";
+}
+
+    @RequestMapping("info/")
+    public Map<String, String> getInfo() {
+        Map<String, String> mp = new HashMap<>();
+        mp.put("姓名", "钟永龙");
+        mp.put("身高", "175cm");
+        mp.put("性别","男");
+        mp.put("学校", "湖南大学");
+        mp.put("mail", "zhongyl@hnu.edu.cn");
+        return mp;
     }
 }
---
+++ web/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div>姓名：{{ name }}</div>
+    <div>性别：{{ gender }}</div>
+    <div>学校：{{ school }}</div>
+    <div>E-mail：  <a href="http://www.baidu.com">{{ email }}</a> </div>
+    <div>身高：{{ height }}</div>
+  </div>
+  <router-view/>
+</template>
+
+<script>
+import $ from "jquery";
+import { ref } from "vue";
+
+export default {
+  name: "App",
+  setup: () => {
+    let name = ref("");
+    let gender = ref("");
+    let school = ref("");
+    let email = ref("");
+    let height = ref("");
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/info/",
+      type: "get",
+      success: resp => {
+        console.log(resp);
+        name.value = resp.姓名;
+        gender.value = resp.性别;
+        school.value = resp.学校;
+        email.value = resp.mail;
+        height.value = resp.身高;
+      }
+    });
+
+    return {
+      name,
+      gender,
+      school,
+      email,
+      height,
+    }
+  }
+}
+
+</script>
+
+<style>
+</style>
